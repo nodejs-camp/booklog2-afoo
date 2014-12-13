4a8ba67d617498c634b9c13bd4d4de9fa30d710c
afooo <foofoo.chang@gmail.com>
2014-12-13 23:17:34 +0800

    use workflow, #5

--- routes/posts.js
+++ routes/posts.js
@@ -16,17 +16,39 @@ exports.list = function(req, res){
 exports.create = function(req, res){
 	var workflow = new events.EventEmitter();
 	var model = req.app.db.model.Post;
-	var title = req.query.title;
-	var content = req.query.content;
+	var title = req.body.title;
+	var content = req.body.content;
 
-	var post = new model({
+	workflow.outcome = {
+		success: false,
+		errfor: {}
+	};
+	
+	workflow.on('validate', function(){
+		if(title.length === 0)
+			workflow.outcome.errfor.title = '這是必填欄位';
+
+		if(content.length === 0)
+			workflow.outcome.errfor.content = '這是必填欄位';
+
+		// if has error, response error
+		if(Object.keys(workflow.outcome.errfor).length !== 0)
+			return workflow.emit('response');
+
+		workflow.emit('savePost');
+	})
+
+	workflow.on('savePost', function(){
+		var post = new model({
 			title: title,
 			content: content
+		});
+		post.save();
+
+		workflow.emit('response');
 	});
 
-	post.save();
-
-	res.send({
-		status: 'OK'
+	workflow.on('response', function(){
+		res.send(workflow.outcome);
 	});
 };
