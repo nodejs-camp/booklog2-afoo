97da61a60bc65afd3f8983e944fc2a369bfbe0fa
afooo <foofoo.chang@gmail.com>
2014-12-28 14:50:34 +0800

    remove FB callback URI fix, #15

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // must login
+/*
 router.get('/', function(req, res, next) {
  	if (req.isAuthenticated()) {
 		next();
@@ -9,6 +10,7 @@ router.get('/', function(req, res, next) {
 		res.redirect('/login');
 	}
 });
+*/
 
 /* GET home page. */
 router.get('/', function(req, res) {
